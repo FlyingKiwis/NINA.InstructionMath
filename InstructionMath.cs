d4c04b6175a20e35606a7cb5969923220f749105
Drew M <39811322+FlyingKiwis@users.noreply.github.com>
2022-08-17 17:41:42 -0400

    Add license check

--- InstructionMath.cs
+++ InstructionMath.cs
@@ -19,13 +19,7 @@ using System.Threading.Tasks;
 using Settings = NINA.InstructionMath.Properties.Settings;
 
 namespace NINA.InstructionMath {
-    /// <summary>
-    /// This class exports the IPluginManifest interface and will be used for the general plugin information and options
-    /// The base class "PluginBase" will populate all the necessary Manifest Meta Data out of the AssemblyInfo attributes. Please fill these accoringly
-    /// 
-    /// An instance of this class will be created and set as datacontext on the plugin options tab in N.I.N.A. to be able to configure global plugin settings
-    /// The user interface for the settings will be defined by a DataTemplate with the key having the naming convention "InstructionMath_Options" where InstructionMath corresponds to the AssemblyTitle - In this template example it is found in the Options.xaml
-    /// </summary>
+
     [Export(typeof(IPluginManifest))]
     public class InstructionMath : PluginBase, INotifyPropertyChanged {
 
@@ -36,6 +30,9 @@ namespace NINA.InstructionMath {
                 Settings.Default.UpdateSettings = false;
                 CoreUtil.SaveSettings(Settings.Default);
             }
+
+            var license = org.mariuszgromada.math.mxparser.License.iConfirmNonCommercialUse("Drew McDermott");
+            Logger.Trace($"License approved={license}"); 
         }
 
         public override Task Teardown() {
